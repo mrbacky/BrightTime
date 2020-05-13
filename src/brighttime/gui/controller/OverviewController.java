828322867e9065392861aa0cea9bb64f086c4b81
Rado <radoslav.backovsky@gmail.com>
2020-05-14 00:37:38 +0200

    overview try
    in progress

--- src/brighttime/gui/controller/OverviewController.java
+++ src/brighttime/gui/controller/OverviewController.java
@@ -4,6 +4,7 @@ import brighttime.be.Client;
 import brighttime.be.Filter;
 import brighttime.be.Project;
 import brighttime.be.Task;
+import brighttime.be.TaskEntry;
 import brighttime.gui.model.ModelException;
 import brighttime.gui.model.interfaces.IMainModel;
 import brighttime.gui.util.AlertManager;
@@ -13,15 +14,30 @@ import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import java.net.URL;
 import java.time.DayOfWeek;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.YearMonth;
 import java.time.temporal.TemporalAdjusters;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.StackedBarChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -34,6 +50,13 @@ import javafx.scene.layout.AnchorPane;
  */
 public class OverviewController implements Initializable {
 
+    private final XYChart.Series<String, Integer> billableSeries = new XYChart.Series<>();
+    private final XYChart.Series<String, Integer> nonBillableSeries = new XYChart.Series<>();
+
+    private final ObservableList<XYChart.Series<String, Integer>> data = FXCollections.observableArrayList();
+
+//    private final ObservableList<XYChart.Data<String, Number>> billableData = FXCollections.observableArrayList();
+//    private final ObservableList<XYChart.Data<String, Number>> nonBillableData = FXCollections.observableArrayList();
     @FXML
     private TableView<Task> tbvTasks;
 
@@ -69,6 +92,12 @@ public class OverviewController implements Initializable {
     private JFXButton btnLastWeek;
     @FXML
     private JFXButton btnLastMonth;
+    @FXML
+    private StackedBarChart<String, Integer> chartTasks;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
 
     //TODO: Very basic and lacking implementation made to check the connection between the view and database.
     public OverviewController() {
@@ -93,6 +122,10 @@ public class OverviewController implements Initializable {
         selectProject();
         listenDatePickerStart();
         listenDatePickerEnd();
+
+//        setupBarCharSeries();
+//        initHardcodedChart();
+//        initChartData();
     }
 
     void injectMainModel(IMainModel mainModel) {
@@ -179,6 +212,7 @@ public class OverviewController implements Initializable {
             if (newValue != null && dpStartDate.getValue() != null) {
                 try {
                     mainModel.getAllTasksFiltered(new Filter(cboProjects.getValue(), dpStartDate.getValue(), newValue));
+                    initChartData();
                 } catch (ModelException ex) {
                     alertManager.showAlert("Could not filter by date.", "An error occured: " + ex.getMessage());
                 }
@@ -242,9 +276,80 @@ public class OverviewController implements Initializable {
             dpEndDate.setValue(lastDayOfLastMonth);
 
             mainModel.getAllTasksFiltered(new Filter(cboProjects.getValue(), dpStartDate.getValue(), dpEndDate.getValue()));
+
         } catch (ModelException ex) {
             alertManager.showAlert("Could not filter in this month", "An error occured: " + ex.getMessage());
         }
+    }
+
+    private ObservableList<Series<String, Integer>> initChartData() {
+
+        ObservableList<Task> taskList = mainModel.getTaskList();
+
+        LocalDate startDate = dpStartDate.getValue();
+        LocalDate endDate = dpEndDate.getValue();
+//        long daysForChart = Duration.between(startDate, endDate).toDaysPart();
+
+        Map<LocalDate, List<Task>> taskMap = mainModel.getTasks();
+        Map<LocalDate, List<Task>> filteredMap = new HashMap<>();
+        for (Map.Entry<LocalDate, List<Task>> entry : taskMap.entrySet()) {
+            LocalDate key = entry.getKey();
+            List<Task> value = entry.getValue();
+            filteredMap = (Map<LocalDate, List<Task>>) taskMap.entrySet().stream().filter(allTasks
+                    -> allTasks.getKey().isAfter(startDate) || allTasks.getKey().isBefore(endDate))
+                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        }
+
+        for (Map.Entry<LocalDate, List<Task>> entry : filteredMap.entrySet()) {
+            List<Task> value = entry.getValue();
+            System.out.println("task: " + value);
+
+        }
+//
+//        List<Task> billableTasks = taskList.stream()
+//                .filter(task -> task.getBillability().equals(Task.Billability.BILLABLE))
+//                .collect(Collectors.toList());
+////                -> allTasks.getBillability().equals(Task.Billability.BILLABLE)).collect(Collectors.toList());
+//        ObservableList<Task> billableTasksObsList = FXCollections.observableArrayList(billableTasks);
+//        List<Task> nonBillableTasks = taskList.stream()
+//                .filter(task -> task.getBillability().equals(Task.Billability.NON_BILLABLE))
+//                .collect(Collectors.toList());
+//        ObservableList<Task> nonBillableTasksObsList = FXCollections.observableArrayList(nonBillableTasks);
+//        int billableHoursSum = 0;
+//        for (Task task : billableTasksObsList) {
+//            billableHoursSum += task.getTotalDuration();
+//        }
+//        System.out.println("billableHoursSum: " + billableHoursSum);
+//        int nonBillableHoursSum = 0;
+//        for (Task task : nonBillableTasksObsList) {
+//            nonBillableHoursSum += task.getTotalDuration();
+//        }
+//        System.out.println("nonBillableHoursSum: " + nonBillableHoursSum);
+////  how many days, that many runs in the loop
+////  in one day are tasks with billable and nonbillable status
+//
+//        data.addAll(billableSeries, nonBillableSeries);
+//        chartTasks.getData().addAll(data);
+        return null;
+    }
+
+    private void setupBarCharSeries() {
+        billableSeries.setName("Billable");
+        chartTasks.getData().add(billableSeries);
+
+        nonBillableSeries.setName("Nonbillable");
+        chartTasks.getData().add(nonBillableSeries);
+
+    }
+
+    private void initHardcodedChart() {
+
+    }
+
+    private List<TaskEntry> getEntriesForDay(Task task, LocalDate date) {
+        List<TaskEntry> dayEntries = task.getTaskEntryList().stream().filter(allEntries
+                -> allEntries.getStartTime().toLocalDate().equals(date)).collect(Collectors.toList());
+        return dayEntries;
     }
 
 }
